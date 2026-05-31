909e30bed7fe807d8517ebc0b78c3146e2ea3b1b
spyflow <javier.ferrada2201@alumnos.ubiobio.cl>
2026-05-31 18:11:58 -0400

    Add license info and enhance error handling
    Added licensing information and improved error handling for command line arguments.

--- code.c
+++ code.c
@@ -1,3 +1,21 @@
+/*
+ * oBattery - Simple Linux battery check percentage calculator
+ * Copyright (C) 2026 spyflow
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -30,6 +48,10 @@ int main(int argc, char *argv[]) {
                 printf("Error: --precision requires a number argument.\n");
                 return 1;
             }
+        }else {
+            fprintf(stderr, "Unknown option: %s\n", argv[i]);
+            //show_help();
+            return 1;
         }
     }
 
